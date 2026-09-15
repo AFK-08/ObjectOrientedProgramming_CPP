fc5112b489ebb6c8d290ed0d2f4d3298eb58dbe2
Ahmad Farooq <ahmadfkwj@gmail.com>
2026-09-16 01:27:48 +0500

    Task 1 completed using pointers and dynamic memory

--- Lab_01/getManyIntegers.cpp
+++ Lab_01/getManyIntegers.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 using namespace std;
+
+int* getManyIntegers(int& count)
+{
+	int arr[100];
+	int i = 0;
+	while (i < 100 && arr[i] != -999)
+	{
+		i++;
+		cout << "Enter an integer: ";
+		cin >> arr[i];
+		count++;
+	}
+	int* numbers = new int[count];
+	for (int i = 1; i <  count; i++)
+	{
+		numbers[i] = arr[i];
+	}
+	return numbers;
+}
+
 int main()
 {
-    cout<<"Hello world"<<endl;
+	int count = 0;
+	int* outputArray = getManyIntegers(count);
+	for (int i = 1;i < count;i++)
+	{
+		cout << outputArray[i] << endl;
+	}
+	delete []outputArray;
+	outputArray = NULL;
+	return 0;
 }
